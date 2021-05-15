6d1ee48d608f35a8c8ef9fd037dcdb1be6cbed2c
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-05-15 21:55:07 +0300

    formula added

---
+++ src/main/java/com/mentalhealth/app/domain/Formula.java
@@ -0,0 +1,33 @@
+package com.mentalhealth.app.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table (name = "formula")
+@Getter
+@Setter
+public class Formula extends AbstractAuditingEntity implements Serializable {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Size (max = 255)
+    @Column(name = "name")
+    private String name;
+
+    @Size (max = 4000)
+    @Column(name = "formula", length = 4000)
+    private String value;
+
+    @Size (max = 4000)
+    @Column (name = "variables", length = 4000)
+    private String variables;
+}
