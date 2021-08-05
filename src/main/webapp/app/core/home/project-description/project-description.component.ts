6ef7429ab222344020d93200444124b42b086090
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-08-06 00:08:30 +0300

    buttons are added to main page

--- src/main/webapp/app/core/home/project-description/project-description.component.ts
+++ src/main/webapp/app/core/home/project-description/project-description.component.ts
@@ -1,5 +1,13 @@
 import Component from 'vue-class-component';
-import { Vue } from 'vue-property-decorator';
+import {Inject, Vue} from 'vue-property-decorator';
+import LoginService from "@/account/login.service";
 
 @Component
-export default class ProjectDescriptionComponent extends Vue {}
+export default class ProjectDescriptionComponent extends Vue {
+    @Inject('loginService')
+    private loginService: () => LoginService;
+
+    public openLogin(): void {
+        this.loginService().openLogin((<any>this).$root);
+    }
+}
--- src/main/webapp/app/core/home/project-description/project-description.vue
+++ src/main/webapp/app/core/home/project-description/project-description.vue
@@ -1,9 +1,15 @@
 <template>
     <header id="projectDescription" class="project-description">
+      <div class="h-100 d-flex justify-content-center">
+        <div id="button-content" class="align-self-end">
+          <button class="btn btn-danger" v-text="$t('login.title')" v-on:click="openLogin()">Login</button>
+          <button class="btn btn-secondary" v-text="$t('register.form.button')" @click="$router.push('register')">Register</button>
+        </div>
+      </div>
     </header>
 </template>
 
 <script lang="ts" src="./project-description.component.ts">
 </script>
-<style lang="scss" src="./project-description.scss">
+<style lang="scss" src="./project-description.scss" scoped>
 </style>
