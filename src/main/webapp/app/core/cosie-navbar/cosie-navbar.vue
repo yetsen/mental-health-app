f54e74565153d0680b3f7d0d57d8499a585d21c3
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-08-12 19:43:16 +0300

    ui design change started

--- src/main/webapp/app/core/cosie-navbar/cosie-navbar.vue
+++ src/main/webapp/app/core/cosie-navbar/cosie-navbar.vue
@@ -18,6 +18,16 @@
             <li v-bind:class="{'current-menu-item': currentRoute == 'SurveyCard'}"  v-if="currentTime === 1 && isInHomePage() && authenticated" @click="onAssessmentClick(currentTime)" class="menu-item menu-item-75" id="menu_item_75">
               <a href="#">Assessment Center</a>
             </li>
+            <li class="menu-item menu-item-120" id="menu_item_120" v-if="currentTime > 1 && isInHomePage() && authenticated">
+              <a>Assessment Center</a>
+              <ul>
+                <li v-for="si in surveyInformation" v-bind:key="si.times" @click="onAssessmentClick(si.times)">
+                  <a>Assessment #{{si.times}}</a>
+                  <font-awesome-icon style="color: white" v-if="si.finished" icon="check" />
+                  <font-awesome-icon v-if="!si.finished" icon="check" />
+                </li>
+              </ul>
+            </li>
             <li v-bind:class="{'current-menu-item': currentRoute == 'Consortium'}"  class="menu-item menu-item-2416" id="menu_item_2416">
               <a href="/consortium">Consortium</a>
             </li>
@@ -26,6 +36,26 @@
             </li>
             <li v-bind:class="{'current-menu-item': currentRoute == 'Contact'}"  class="menu-item menu-item-143" id="menu_item_143">
               <a href="/contact-us">Contact us</a>
+            </li>
+            <li class="menu-item menu-item-140" id="menu_item_140">
+              <a >Profile</a>
+              <ul>
+                <li v-if="!authenticated">
+                  <a href="#" v-on:click="openLogin()">Sign in</a>
+                </li>
+                <li v-if="!authenticated">
+                  <a href="/account/settings">Register</a>
+                </li>
+                <li v-if="authenticated">
+                  <a href="/register">Settings</a>
+                </li>
+                <li v-if="authenticated">
+                  <a href="/account/password">Password</a>
+                </li>
+                <li v-if="authenticated">
+                  <a v-on:click="logout()" href="#">Sign out</a>
+                </li>
+              </ul>
             </li>
           </ul>
           <div class="some-buttons-wrap show-for-small-only">
