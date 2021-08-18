9129f8aec0d199661f38e1886c20a20a612e76cb
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-08-19 00:34:50 +0300

    ui design change started

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -4,27 +4,22 @@
         <div class=columns>
           <div class=row-navbar>
             <div class="small-12 columns component-content">
-              <p style="font-size: 0.75rem;"><a href="http://www.cosie-project.eu/">www.cosie-project.eu</a><br/>
-                #cosie2020<br/>
-                <a href="mailto:info@cosie-project.eu">info@cosie-project.eu</a><br/>
-                Project reference: 770492<br/>
-                Implementation: 1.12.2017- 31.5.2021<br/>
-                H2020-SC6-COCREATION-2017</p>
+              <img loading=lazy class="alignnone wp-image-532" src="/content/images/cosie/amblem.png" alt="" width=152 height=101 />
             </div>
           </div>
         </div>
         <div class=columns>
           <div class=row-navbar>
             <div class="small-12 columns component-content">
-              <p><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:240}"> <img loading=lazy class="alignnone wp-image-532" src="/content/images/cosie/eu-emblem.png" alt="" width=152 height=101 />  </span></p>
+
             </div>
           </div>
         </div>
         <div class=columns>
           <div class=row-navbar>
             <div class="small-12 columns component-content">
-              <p style="font-size: 0.75rem;">This project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement No 770492.</p>
-              <p style="font-size: 0.75rem;">The content of the web site reflects the authors’ views and the Managing Agency cannot be held responsible for any use that may be made of the information it contains.</p>
+              <p style="font-size: 0.75rem;">This project has received funding from UKRI- UK Research and Innovation under project ID number ES/V004263/1
+              </p>
             </div>
           </div>
         </div>
