ecfbfa79eaeca75b27746af890455bb14c3f557b
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-08-22 12:17:26 +0300

    dashboard dark theme implemented

--- src/main/webapp/app/core/dashboard/dummy/dummy.component.ts
+++ src/main/webapp/app/core/dashboard/dummy/dummy.component.ts
@@ -18,7 +18,7 @@ Highcharts.createElement('link', {
   type: 'text/css'
 }, null, document.getElementsByTagName('head')[0]);
 
-Highcharts.theme = {
+Highcharts['theme'] = {
   colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
     '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'
   ],
@@ -219,7 +219,7 @@ Highcharts.theme = {
 };
 if (true) {
   // Apply the theme
-  Highcharts.setOptions(Highcharts.theme);
+  Highcharts.setOptions(Highcharts['theme']);
 }
 
 @Component
