00c2bd67316f76e07d784a49cac67d21cf482f2b
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-09-01 02:53:05 +0300

    home page implemented

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -1,81 +1,18 @@
 <template>
   <main id="main-content" role="main">
-    <div class="row-navbar">
-      <div class="small-12 large-8 columns">
-        <div class="row-navbar component" data-equalizer="latest-content">
-          <div class="small-12 columns">
-            <h3 class="latest-heading">CoSIE Blog</h3>
-            <div class="latest_posts_component" data-equalizer="latest-header">
-              <div data-offset="2" class="row-navbar latest-post-row">
-                <div class="small-12 medium-6 columns">
-                  <div class="latest-post-row__content">
-                    <h3><a href="indexdfaf.html?post_type=post&amp;p=3376">Intangible Presents to all CoSIE Folks</a></h3>
-                    <p>The Co-creation of Service Innovation in Europe (CoSIE) project states that public service innovations can be achieved by creating collaborative partnerships between service providers and service......</p><a href="indexdfaf.html?post_type=post&amp;p=3376" aria-hidden="true" tabindex="-1" class="button">Read more</a>
-                  </div>
-                </div>
-                <div class="small-12 medium-6 columns">
-                  <a href="indexdfaf.html?post_type=post&amp;p=3376" aria-hidden="true" tabindex="-1"><img src="/content/images/cosie/cosie_street-768x431.jpg" /></a>
-                </div>
-              </div>
-              <div data-offset="2" class="row-navbar latest-post-row">
-                <div class="small-12 medium-6 columns">
-                  <div class="latest-post-row__content">
-                    <h3><a href="index1670.html?post_type=post&amp;p=2605">Co-creation, Digital Technologies and COVID-19</a></h3>
-                    <p>Can COVID-19 help to introduce a paradigm shift in the use of digital technologies to assist co-creation in disability services? This is one of the slightly surprising themes that evolved from a......</p><a href="index1670.html?post_type=post&amp;p=2605" aria-hidden="true" tabindex="-1" class="button">Read more</a>
-                  </div>
-                </div>
-                <div class="small-12 medium-6 columns">
-                  <a href="index1670.html?post_type=post&amp;p=2605" aria-hidden="true" tabindex="-1"><img src="/content/images/cosie/cosie_old_woman-768x431.jpg" /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="small-12 columns">
-          <div id="load-wrap-1" class="load-more-wrap">
-            <button class="only-button load-more-button" id="1" data-offset="2" data-posts-in-cats="40" data-current="1" data-layout="row">Load more <i class="fa fa-arrow-down" aria-hidden="true"></i> <span class="spinner display-none"><i class="fa fa-spinner fa-spin" aria-hidden="true"></i></span></button>
-          </div>
-        </div>
-        <div class="row-navbar component" data-equalizer="latest-content">
-          <div class="small-12 columns">
-            <h3 class="latest-heading">CoSIE News</h3>
-            <div id="latest-wrap-3" class="latest_posts_component" data-equalizer="latest-header">
-              <div id="latest-wrap-3" data-offset="2" class="row-navbar latest-post-row">
-                <div class="small-12 medium-6 columns">
-                  <div class="latest-post-row__content">
-                    <h3><a href="index6589.html?post_type=post&amp;p=2617">Seminar: Towards Co-creation of Public Services at the Local Level – Innovative Concepts and Practice Cross Europe</a></h3>
-                    <p>Online seminar on 5 May 2021 - Registration is now open!What are the key issues around co-creation of public services at the local level across Europe? Which challenges, potentials and characteristics......</p><a href="index6589.html?post_type=post&amp;p=2617" aria-hidden="true" tabindex="-1" class="button">Read more</a>
-                  </div>
-                </div>
-                <div class="small-12 medium-6 columns">
-                  <a href="index6589.html?post_type=post&amp;p=2617" aria-hidden="true" tabindex="-1"><img src="/content/images/cosie/cosie_street-768x431.jpg" /></a>
-                </div>
-              </div>
-              <div id="latest-wrap-3" data-offset="2" class="row-navbar latest-post-row">
-                <div class="small-12 medium-6 columns">
-                  <div class="latest-post-row__content">
-                    <h3><a href="index7481.html?post_type=post&amp;p=2581">Webinar: Co-creation of Services of General Interest: the Role of Citizens and Their Organizations</a></h3>
-                    <p>We kindly invite you to our seminar onCo-creation of Services of General Interest: the Role of Citizens and their Organizations15 April 2021, 2.30-6 pm (CET)The seminar is organized by the European......</p><a href="index7481.html?post_type=post&amp;p=2581" aria-hidden="true" tabindex="-1" class="button">Read more</a>
-                  </div>
-                </div>
-                <div class="small-12 medium-6 columns">
-                  <a href="index7481.html?post_type=post&amp;p=2581" aria-hidden="true" tabindex="-1"><img src="/content/images/cosie/cosie_hands-1-768x431.jpg" /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="small-12 columns">
-          <div id="load-wrap-3" class="load-more-wrap">
-            <button class="only-button load-more-button" id="3" data-offset="2" data-posts-in-cats="32" data-current="1" data-layout="row">Load more <i class="fa fa-arrow-down" aria-hidden="true"></i> <span class="spinner display-none"><i class="fa fa-spinner fa-spin" aria-hidden="true"></i></span></button>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p style="text-align: center">This project is developed to give employees the ability to check how their mental health, well-being and work-related conditions affect their productivity. </p>
+          <p></p>
+          <div class="col-lg-12 start-item">
+            <a class="start-link">
+              <img class="img-responsive" src="/content/images/homepage.png" alt="Mental-Health">
+            </a>
           </div>
         </div>
       </div>
-      <div class="small-12 large-4 columns">
-        <div class="row-navbar component">
-          <div class="small-12 columns"></div>
-        </div>
-      </div>
+
     </div>
   </main>
 </template>
