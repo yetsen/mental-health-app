d44c9575580736f50b06250711ad75f820d47423
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-09-05 01:13:52 +0300

    ui improvements

---
+++ src/main/webapp/app/core/work-package/work-package.vue
@@ -0,0 +1,91 @@
+<template>
+  <!-- ======= Features Section ======= -->
+  <section id="features" class="features">
+
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <h2>Features</h2>
+        <p>Laboriosam et omnis fuga quis dolor direda fara</p>
+      </header>
+      <!-- Feature Tabs -->
+      <div class="row feture-tabs" data-aos="fade-up">
+        <div class="col-lg-6">
+          <h3>Neque officiis dolore maiores et exercitationem quae est seda lidera pat claero</h3>
+
+          <!-- Tabs -->
+          <ul class="nav nav-pills mb-3">
+            <li>
+              <a class="nav-link active" data-bs-toggle="pill" href="#tab1">Saepe fuga</a>
+            </li>
+            <li>
+              <a class="nav-link" data-bs-toggle="pill" href="#tab2">Voluptates</a>
+            </li>
+            <li>
+              <a class="nav-link" data-bs-toggle="pill" href="#tab3">Corrupti</a>
+            </li>
+          </ul><!-- End Tabs -->
+
+          <!-- Tab Content -->
+          <div class="tab-content">
+
+            <div class="tab-pane fade show active" id="tab1">
+              <p>Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+              <div class="d-flex align-items-center mb-2">
+                <i class="bi bi-check2"></i>
+                <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
+              </div>
+              <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
+              <div class="d-flex align-items-center mb-2">
+                <i class="bi bi-check2"></i>
+                <h4>Incidunt non veritatis illum ea ut nisi</h4>
+              </div>
+              <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
+            </div><!-- End Tab 1 Content -->
+
+            <div class="tab-pane fade show" id="tab2">
+              <p>Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+              <div class="d-flex align-items-center mb-2">
+                <i class="bi bi-check2"></i>
+                <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
+              </div>
+              <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
+              <div class="d-flex align-items-center mb-2">
+                <i class="bi bi-check2"></i>
+                <h4>Incidunt non veritatis illum ea ut nisi</h4>
+              </div>
+              <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
+            </div><!-- End Tab 2 Content -->
+
+            <div class="tab-pane fade show" id="tab3">
+              <p>Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+              <div class="d-flex align-items-center mb-2">
+                <i class="bi bi-check2"></i>
+                <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
+              </div>
+              <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
+              <div class="d-flex align-items-center mb-2">
+                <i class="bi bi-check2"></i>
+                <h4>Incidunt non veritatis illum ea ut nisi</h4>
+              </div>
+              <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
+            </div><!-- End Tab 3 Content -->
+
+          </div>
+
+        </div>
+
+        <div class="col-lg-6">
+          <img src="assets/img/features-2.png" class="img-fluid" alt="">
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script lang="ts" src="./work-package.component.ts">
+</script>
+
+<style lang="scss" src="src/main/webapp/app/core/work-package/work-package.scss" scoped></style>
