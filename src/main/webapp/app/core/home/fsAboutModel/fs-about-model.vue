18a07ce1e46641c36f1a8d7528c762887b9db24a
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-09-06 00:32:24 +0300

    ui improvements

--- src/main/webapp/app/core/home/fsAboutModel/fs-about-model.vue
+++ src/main/webapp/app/core/home/fsAboutModel/fs-about-model.vue
@@ -22,8 +22,8 @@
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
           <div class="service-box orange">
             <i class="ri-discuss-line icon"></i>
-            <h3>Eosle Commodi</h3>
-            <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
+            <h3>Objectives</h3>
+            <p>Develop a conceptual model that will facilitate analysing the relationship between MH issues faced by SMEs’ staff, factors</p>
             <a  @click="goTo('/work-package/2')" class="read-more"><a>Read More</a> <i class="bi bi-arrow-right"></i></a>
           </div>
         </div>
@@ -31,8 +31,8 @@
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
           <div class="service-box green">
             <i class="ri-discuss-line icon"></i>
-            <h3>Ledo Markt</h3>
-            <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+            <h3>Work Package 1</h3>
+            <p>The conceptual model will examine  the impact of staff MH problems, well-being issues, and various factors</p>
             <a @click="goTo('/work-package/3')" class="read-more"><a>Read More</a> <i class="bi bi-arrow-right"></i></a>
           </div>
         </div>
@@ -40,8 +40,8 @@
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
           <div class="service-box red">
             <i class="ri-discuss-line icon"></i>
-            <h3>Asperiores Commodi</h3>
-            <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+            <h3>Work Package 2</h3>
+            <p>The deliverables of WP1 (survey and model), guidance published by policy makers and findings from management</p>
             <a  @click="goTo('/work-package/4')" class="read-more"><a>Read More</a> <i class="bi bi-arrow-right"></i></a>
           </div>
         </div>
@@ -49,8 +49,8 @@
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
           <div class="service-box purple">
             <i class="ri-discuss-line icon"></i>
-            <h3>Velit Doloremque.</h3>
-            <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
+            <h3>Work Package 3</h3>
+            <p>Develop and test a user-friendly gamified application enabling SMEs’ staff to anonymously and remotely answer</p>
             <a  @click="goTo('/work-package/5')" class="read-more"><a>Read More</a> <i class="bi bi-arrow-right"></i></a>
           </div>
         </div>
@@ -58,8 +58,8 @@
         <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="700">
           <div class="service-box pink">
             <i class="ri-discuss-line icon"></i>
-            <h3>Dolori Architecto</h3>
-            <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
+            <h3>Work Package 4</h3>
+            <p>The primary objective is to examine the impact and effectiveness of the framework in a longitudinal study</p>
             <a @click="goTo('/work-package/6')" class="read-more"><a>Read More</a> <i class="bi bi-arrow-right"></i></a>
           </div>
         </div>
