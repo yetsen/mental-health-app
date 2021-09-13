4c48036fa365590c2f463cc04b496519082a503e
Yunus Emre Tekin <emre.tekin@pronetgaming.com>
2021-09-13 22:19:48 +0300

    consortium rearranged

--- src/main/webapp/app/core/home/fsConsortium/fsConsortium.vue
+++ src/main/webapp/app/core/home/fsConsortium/fsConsortium.vue
@@ -8,6 +8,9 @@
         <h2>Consortium</h2>
       </header>
 
+
+      <h1>Aston University</h1>
+
       <div class="row gy-4">
 
         <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
@@ -39,62 +42,6 @@
         <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
           <div class="member">
             <div class="member-img">
-              <img src="/content/images/team/Professor-Carole-Howorth.jpg" class="img-fluid" alt="">
-
-            </div>
-            <div class="member-info">
-              <h4>Prof Carole Howorth</h4>
-              <span>University of York</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-          <div class="member">
-            <div class="member-img">
-              <img src="/content/images/team/Carolyn-Chew-Graham.jpg" class="img-fluid" alt="">
-
-            </div>
-            <div class="member-info">
-              <h4>Prof Carolyn Chew-Graham</h4>
-              <span>Keele University</span>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <br/>
-      <div class="row gy-4">
-
-        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-          <div class="member">
-            <div class="member-img">
-              <img src="/content/images/team/Peter-Ball.jpg" class="img-fluid" alt="">
-
-            </div>
-            <div class="member-info">
-              <h4>Prof Peter Ball</h4>
-              <span>University of York</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-          <div class="member">
-            <div class="member-img">
-              <img src="/content/images/team/marwaha-stephen.jpg" class="img-fluid" alt="">
-
-            </div>
-            <div class="member-info">
-              <h4>Prof Steven Marwaha</h4>
-              <span>University of Birmingham</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-          <div class="member">
-            <div class="member-img">
               <img src="/content/images/team/Soumyadeb_Chowdhury.jpg" class="img-fluid" alt="">
 
             </div>
@@ -104,7 +51,6 @@
             </div>
           </div>
         </div>
-
 
         <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
           <div class="member">
@@ -139,6 +85,59 @@
         <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
           <div class="member">
             <div class="member-img">
+              <img src="/content/images/team/nur_gundogdu.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>Dr Nur Gundogdu</h4>
+              <span>Aston University</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+
+        </div>
+
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+
+        </div>
+
+      </div>
+      <br/>
+      <br/>
+      <h1>University of York</h1>
+      <div class="row gy-4">
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+          <div class="member">
+            <div class="member-img">
+              <img src="/content/images/team/Professor-Carole-Howorth.jpg" class="img-fluid" alt="">
+
+            </div>
+            <div class="member-info">
+              <h4>Prof Carole Howorth</h4>
+              <span>University of York</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+          <div class="member">
+            <div class="member-img">
+              <img src="/content/images/team/peter_ball.jpg" class="img-fluid" alt="">
+
+            </div>
+            <div class="member-info">
+              <h4>Prof Peter Ball</h4>
+              <span>University of York</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+          <div class="member">
+            <div class="member-img">
               <img src="/content/images/team/Jane-Suter.jpg" class="img-fluid" alt="">
 
             </div>
@@ -148,35 +147,67 @@
             </div>
           </div>
         </div>
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
 
-        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-          <div class="member">
-            <div class="member-img">
-              <img src="/content/images/team/Tri-Dung-Nguyen.jpg" class="img-fluid" alt="">
-
-            </div>
-            <div class="member-info">
-              <h4>Dr Tri Dung Nguyen</h4>
-              <span>Aston University</span>
-            </div>
-          </div>
         </div>
-
-        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-          <div class="member">
-            <div class="member-img">
-              <img src="/content/images/team/nur_gundogdu.jpg" class="img-fluid" alt="">
-
-            </div>
-            <div class="member-info">
-              <h4>Dr Nur Gundogdu</h4>
-              <span>Aston University</span>
-            </div>
-          </div>
-        </div>
-
       </div>
       <br/>
+      <br/>
+      <h1>University of Birmingham</h1>
+      <div class="row gy-4">
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+          <div class="member">
+            <div class="member-img">
+              <img src="/content/images/team/marwaha-stephen.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>Prof Steven Marwaha</h4>
+              <span>University of Birmingham</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+
+        </div>
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <h1>Keele University</h1>
+      <div class="row gy-4">
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+          <div class="member">
+            <div class="member-img">
+              <img src="/content/images/team/Carolyn-Chew-Graham.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>Prof Carolyn Chew-Graham</h4>
+              <span>Keele University</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+
+        </div>
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <h1>King’s College London</h1>
       <div class="row gy-4">
         <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
           <div class="member">
@@ -190,10 +221,19 @@
             </div>
           </div>
         </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+
+        </div>
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+
+        </div>
       </div>
-
     </div>
-
   </section><!-- End Team Section -->
 
 </template>
